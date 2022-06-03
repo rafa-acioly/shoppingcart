fe0cc1fc03adb548610f6b89236109a5914097b9
Rafael Acioly <aciolyr@gmail.com>
2022-06-03 13:09:57 -0300

    normalize fake products database

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,10 +1,8 @@
-from decimal import Decimal
+from typing import List
 from shoppingcart.entities import ProductRequest
 
-fake_products = [
-    ProductRequest(product_id="1", quantity=1, price=Decimal("10.00")),
-    ProductRequest(product_id="2", quantity=2, price=Decimal("20.00")),
-    ProductRequest(product_id="3", quantity=3, price=Decimal("30.00")),
-    ProductRequest(product_id="4", quantity=4, price=Decimal("40.00")),
-    ProductRequest(product_id="5", quantity=5, price=Decimal("50.00")),
-]
+
+"""
+fake_products will hold the list of products that were added to the cart.
+"""
+fake_products: List[ProductRequest] = []
