b64b025bd04953cba89e2366b1536832d03a15b0
Rafael Acioly <aciolyr@gmail.com>
2022-06-06 14:15:25 -0300

    adds method to flush cart

--- shoppingcart/main.py
+++ shoppingcart/main.py
@@ -2,7 +2,7 @@ from typing import List
 from fastapi import FastAPI, HTTPException, Path
 from http import HTTPStatus
 
-from tools import fake_cart
+from tools import cart_service as cart_service
 from tools.schemas import ProductRequest, Response
 
 app = FastAPI()
@@ -11,23 +11,23 @@ app = FastAPI()
 @app.get("/cart", response_model=Response)
 async def index() -> Response:
     """List all products that were add to the cart."""
-    return fake_cart
+    return cart_service
 
 
-@app.post("/cart", response_model=Response)
-async def save(product: ProductRequest) -> Response:
-    """Adds a new product to the cart."""
-    fake_cart.add_product(product)
-    return fake_cart
-
-
-@app.patch("/cart/{product_sku}")
-async def update(
+@app.put("/cart/{product_sku}")
+async def upsert(
     product_cart: ProductRequest,
     product_sku: str = Path(description="Unique identifier of the product to update"),
 ) -> Response:
     """Update a product on the cart."""
-    pass
+    cart_service.upsert_product(product_cart)
+    return cart_service
+
+
+@app.delete("/cart", status_code=HTTPStatus.OK)
+async def flush() -> None:
+    """Remove all products from the cart."""
+    cart_service.flush()
 
 
 @app.delete("/cart/{product_sku}", status_code=HTTPStatus.OK)
@@ -35,10 +35,10 @@ async def delete(
     product_sku: str = Path(description="Unique identifier of the product to delete from the cart")  # noqa: E501
 ) -> None:
     """Remove a product from the cart."""
-    if not fake_cart.contains(product_sku):
+    if not cart_service.contains(product_sku):
         raise HTTPException(
             status_code=HTTPStatus.NOT_MODIFIED,
             detail=f"Product with sku: {product_sku} not found on the cart."
         )
 
-    fake_cart.remove_product(product_sku)
+    cart_service.remove_product(product_sku)
